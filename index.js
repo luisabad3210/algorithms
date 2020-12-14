3758f65ce69d9468690b4a0d7309b4b768dae343
Luis <luisabad3210@gmail.com>
2020-12-14 11:29:50 -0500

    680. Valid Palindrome II

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 // console.log('hello world')
 
 // 136. Single Number
-const singleNumber = (nums) => {
-    let result = 0
-    for (let i = 0; i < nums.length; i++) {
-        result ^= nums[i]
-    }
-    return result
-};
-console.log(singleNumber([4,1,2,1,2]));
+// const singleNumber = (nums) => {
+//     let result = 0
+//     for (let i = 0; i < nums.length; i++) {
+//         result ^= nums[i]
+//     }
+//     return result
+// };
+// console.log(singleNumber([4,1,2,1,2]));
 
+// _________________________________________________
 
 // 1619. Mean of Array After Removing Some Elements
-const trimMean = arr => {
-    arr = arr.sort((a, b) => {return a - b})
-    let percent = arr.length * 0.05;
-    let array = arr.splice(percent, arr.length - (percent * 2));
-    console.log(array)
+// const trimMean = arr => {
+//     arr = arr.sort((a, b) => {return a - b})
+//     let percent = arr.length * 0.05;
+//     let array = arr.splice(percent, arr.length - (percent * 2));
+//     console.log(array)
     
-    let len = array.length;
-    let sum = 0
-    for (let i = 0; i < len; i++) {
-        sum += array[i]
+//     let len = array.length;
+//     let sum = 0
+//     for (let i = 0; i < len; i++) {
+//         sum += array[i]
+//     }
+//     return (sum / len).toFixed(5)
+// };
+
+// console.log(trimMean([4,8,4,10,0,7,1,3,7,8,8,3,4,1,6,2,1,1,8,0,9,8,0,3,9,10,3,10,1,10,7,3,2,1,4,9,10,7,6,4,0,8,5,1,2,1,6,2,5,0,7,10,9,10,3,7,10,5,8,5,7,6,7,6,10,9,5,10,5,5,7,2,10,7,7,8,2,0,1,1]))
+
+
+// _________________________________________________
+
+
+// 680. Valid Palindrome II
+// start 10:30
+
+const validPalindrome = s => {
+    reverse = s.split('').reverse().join('');
+    if (reverse === s) {
+        return true;
     }
-    return (sum / len).toFixed(5)
+
+    for (let i = 0; i < s.length; i++) {
+        let x = s.split('').splice(0, i).join('');
+        let y = s.split('').splice(i + 1, s.length).join('');
+        let ori = x + y;
+        let rev = x + y;
+        rev = rev.split('').reverse().join('');
+        if (rev === ori) {
+            return true;
+        }
+    }
+    return false
 };
 
-console.log(trimMean([4,8,4,10,0,7,1,3,7,8,8,3,4,1,6,2,1,1,8,0,9,8,0,3,9,10,3,10,1,10,7,3,2,1,4,9,10,7,6,4,0,8,5,1,2,1,6,2,5,0,7,10,9,10,3,7,10,5,8,5,7,6,7,6,10,9,5,10,5,5,7,2,10,7,7,8,2,0,1,1]))
+console.log(validPalindrome('abagfd'));
+
+
+// end 11:00
